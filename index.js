43228c5a8c5e5630edb77fed32444119167eee29
Arthur Ish <arthurishdev@gmail.com>
2024-01-11 03:28:29 -0800

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+    return Math.abs(42-block);
+}
+
+function distanceFromHqInFeet(block) {
+    return distanceFromHqInBlocks(block) * 264;
+}
+function distanceTravelledInFeet(start, destination){
+    return Math.abs(destination - start) * 264;
+}
+function calculatesFarePrice(start, destination){
+    let feet = distanceTravelledInFeet(start, destination);
+    if (feet <= 400){
+        return 0;
+    } else if (feet <= 2000){
+        return 0.02 * (feet - 400);
+    }
+    else if (feet <= 2500){
+        return 25;
+    }else if (feet > 2500){
+        return "cannot travel that far";
+    }
+     
+}
